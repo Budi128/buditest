46259e76f73c55ebcd6463b38db72261a51599d1
budilutfii <85115264+Budi128@users.noreply.github.com>
2021-09-26 21:48:10 +0700

    Create _autorespon.js

---
+++ plugins/_autorespon.js
@@ -0,0 +1,37 @@
+let fs = require('fs')
+let handler = m => m
+
+handler.all = async function (m, { isBlocked }) {
+
+    if (isBlocked) return
+    if (m.isBaileys) return
+    if (m.chat.endsWith('broadcast')) return
+    let setting = db.data.settings[this.user.jid]
+    let { isBanned } = db.data.chats[m.chat]
+    let { banned } = db.data.users[m.sender]
+
+        }
+    }
+
+    // update status
+    if (new Date() * 1 - setting.status > 1000) {
+        let _uptime = process.uptime() * 1000
+        let uptime = clockString(_uptime)
+        await this.setStatus(`Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} | stikerinbot oleh ariffb`).catch(_ => _)
+        setting.status = new Date() * 1
+    }
+
+}
+
+module.exports = handler
+
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
+
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)]
+}
